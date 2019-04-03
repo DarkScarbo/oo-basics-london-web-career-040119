474e2ca751cf124ced54f96cda99c6db32ff72bc
Xabier Casán <xabier_abia@hotmail.com>
2019-04-03 12:44:03 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,16 +1,15 @@
-class Book
+class Shoe
 
+  attr_reader :brand
+  attr_accessor :color, :size, :material, :condition
 
-    attr_accessor :author, :page_count, :genre
-    attr_reader :title
-
-
-     def initialize(title)
-      @title = title
-    end
-
-    def turn_page
-      puts "Flipping the page...wow, you read fast!"
-    end
-
+  def initialize(brand)
+    @brand = brand
   end
+
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end
+
+end
